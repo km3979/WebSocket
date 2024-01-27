1e3b8bfaa7782bdb0afc6805f8ba52fa6d49482e
km3979 <108575443+km3979@users.noreply.github.com>
2024-01-27 11:21:09 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,47 +1,34 @@
-const WebSocket = require('ws');
-const http = require('http');
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8"/>
+    <script src="https://softvelum.com/player/releases/sldp-v2.17.5.min.js"></script>
+  </head>
+  <body>
+    <div id="player"></div>
 
-const server = http.createServer((req, res) => {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('WebSocket Server\n');
-});
+    <script type="text/javascript">
+      document.addEventListener("DOMContentLoaded", initPlayer);
 
-const wss = new WebSocket.Server({ server });
+      function initPlayer(){
+        sldpPlayer = SLDP.init({
+          container:           'player',
+          stream_url:          'wss://gcphkmgtm.vnkuvideo.com:8881//apps/13Z8iYf6DzV-ctGDrdPaTA/1706325833/fsd0112',
+          adaptive_bitrate: {
+            initial_rendition: '730p'
+          },
+          buffering:           500,
+          autoplay:            true,
+          width:               1240,
+          height:              720,
+        });
+      };
 
-let onlineUsersCount = 1339; // Khởi tạo số người trực tuyến với giá trị tối thiểu là 1339
-
-wss.on('connection', (ws) => {
-    console.log('Client connected');
-
-    ws.send(JSON.stringify({ onlineUsersCount }));
-
-    ws.on('message', (message) => {
-        console.log(`Received message: ${message}`);
-        if (message === 'getOnlineUsersCount') {
-            ws.send(JSON.stringify({ onlineUsersCount }));
-        }
-    });
-
-    ws.on('close', () => {
-        console.log('Client disconnected');
-    });
-});
-
-server.listen(8080, () => {
-    console.log('WebSocket server is listening on port 8080');
-});
-
-// Logic tăng/giảm số người trực tuyến sau mỗi khoảng thời gian
-setInterval(() => {
-    const randomChange = Math.random() > 0.5 ? 1 : -1;
-    onlineUsersCount += randomChange;
-    broadcastOnlineUsersCount();
-}, 5000);
-
-function broadcastOnlineUsersCount() {
-    wss.clients.forEach((client) => {
-        if (client.readyState === WebSocket.OPEN) {
-            client.send(JSON.stringify({ onlineUsersCount }));
-        }
-    });
-}
+      function removePlayer(){
+        sldpPlayer.destroy(function () {
+          console.log('SLDP Player is destroyed.');
+        });
+      }
+    </script>
+  </body>
+</html>
